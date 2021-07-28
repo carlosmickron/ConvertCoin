85290faaa7a3474d6398fe54feee0bf3062de599
Carlos Mickron <82887923+carlosmickron@users.noreply.github.com>
2021-07-28 20:18:03 -0300

    Projeto final
    Utilização da API de cotação 'https://hgbrasil.com/status/finance' e do plugin 'http'

---
+++ main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+// ***esquema para requisições
+
+import 'dart:convert';
+// ***converter Json
+
+import 'dart:async';
+// ***requisições sem precisar esperar, assincrona pra não travar a aplicação
+
+const request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=073bfe04";
+//***link da API utilizada com chave própria
+
+void main() async {
+  // ***resposta que vai obter da API
+  // ***await para esperar os dados e colocar o async após o main
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HOME(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )), // ***primeiro tema utilizado
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+} // ***método para pegar os dados futuro
+
+class HOME extends StatefulWidget {
+  @override
+  _HOMEState createState() => _HOMEState();
+}
+
+class _HOMEState extends State<HOME> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _realChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = ((dolar * this.dolar) / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = ((euro * this.euro) / dolar).toStringAsFixed(2);
+
+  }
+
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("ConvertCoin"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+          // ***opção de carregamento enquanto faz get
+          future: getData(), // ***recebe o método feito
+          builder: (context, snapshot) {
+            // ***context é um parâmetro necessário e snapshot vai tirar um print
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                    child: Text(
+                  "Carregando dados...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ));
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text(
+                    "Falha ao carregar dados. Conexão interrompida!",
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ));
+                } else {
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(
+                          Icons.monetization_on,
+                          size: 150,
+                          color: Colors.amber,
+                        ),
+                        buildTextField(
+                            "Reais", "R\$ ", realController, _realChanged),
+                        Divider(),
+                        buildTextField(
+                            "Dólares", "US\$ ", dolarController, _dolarChanged),
+                        Divider(),
+                        buildTextField(
+                            "Euros", "€ ", euroController, _euroChanged)
+                      ],
+                    ),
+                  );
+                }
+            }
+          }),
+    );
+  }
+}
+
+Widget buildTextField(String label, String prefix,
+    TextEditingController controller, Function troca) {
+  return TextField(
+    controller: controller,
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.amber),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+    ),
+    style: TextStyle(color: Colors.amber, fontSize: 25),
+    onChanged: troca,
+    keyboardType: TextInputType.number,
+  );
+}
